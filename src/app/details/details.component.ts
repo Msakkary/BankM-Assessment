b587a3b5cba94d3899f23fac86f7da6fd0ef3c21
Msakkary <motaz.sakkary@gmail.com>
2023-03-18 01:37:08 +0200

    :wrench: Handling Details Page Redirection and Rerendring Converter Panel

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ConvertQuery } from '@app/@shared/interfaces/fixer.type';
+import { StoreService } from '@app/@shared/services/store.service';
 
 @Component({
   selector: 'app-details',
@@ -7,7 +9,24 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private store: StoreService
+  ) {}
+  urlData!: string;
+  convertedData!: ConvertQuery;
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.activatedRoute.queryParams.subscribe((params: any) => {
+      if (params) {
+        this.urlData = params;
+      }
+    });
+
+    // will be used for the charts
+    this.store.exchangeState$.subscribe((response) => {
+      this.convertedData = response;
+      console.log(this.convertedData);
+    });
+  }
 }
--- src/app/details/details.module.ts
+++ src/app/details/details.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { DetailsComponent } from './details.component';
 import { DetailsRoutingModule } from './details-routing.module';
 import { ChartComponent } from './chart/chart.component';
+import { NgChartsModule } from 'ng2-charts';
 import { SharedModule } from '@shared';
 
 @NgModule({
-  imports: [CommonModule, DetailsRoutingModule, SharedModule],
+  imports: [CommonModule, DetailsRoutingModule, SharedModule, NgChartsModule],
   declarations: [DetailsComponent, ChartComponent],
   exports: [ChartComponent],
 })
