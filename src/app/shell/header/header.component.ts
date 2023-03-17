50bf298df2fa7579ff380e6ec134ac633accbd99
Msakkary <motaz.sakkary@gmail.com>
2023-03-18 01:29:11 +0200

    :white_check_mark: Header Component and Logo to Assets

--- src/app/shell/header/header.component.ts
+++ src/app/shell/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ConvertQuery } from '@app/@shared/interfaces/fixer.type';
+import { StoreService } from '@app/@shared/services/store.service';
 
 @Component({
   selector: 'app-header',
@@ -9,7 +11,20 @@ import { Router } from '@angular/router';
 export class HeaderComponent implements OnInit {
   menuHidden = true;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private store: StoreService) {}
 
   ngOnInit() {}
+
+  toggleMenu() {
+    this.menuHidden = !this.menuHidden;
+  }
+
+  detailsEU(){
+    const queryEU: ConvertQuery = { amount: 1, from: 'EUR', to: 'USD' };
+    this.store.updateState(queryEU)
+  }
+  detailsEG(){
+    const queryEG: ConvertQuery = { amount: 1, from: 'EUR', to: 'GBP' };
+    this.store.updateState(queryEG);
+  }
 }
