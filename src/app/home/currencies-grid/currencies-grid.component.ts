f7c13ef19ab4c851f8d981f64fe0d9e2356b4874
Msakkary <motaz.sakkary@gmail.com>
2023-03-18 01:34:31 +0200

    :white_check_mark: Currencies Grid Component

--- src/app/home/currencies-grid/currencies-grid.component.ts
+++ src/app/home/currencies-grid/currencies-grid.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ApiService } from '@app/@shared/services/api.service';
+import { ConvertQuery, popularRates } from '@app/@shared/interfaces/fixer.type';
 
 @Component({
   selector: 'app-currencies-grid',
@@ -6,7 +8,27 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./currencies-grid.component.scss'],
 })
 export class CurrenciesGridComponent implements OnInit {
-  constructor() {}
+  resultRates!: popularRates;
+  base!: string;
+  amount!: number;
+
+  @Input() set data(value: ConvertQuery) {
+    if (value) {
+      this.base = value.from;
+      this.amount = value.amount;
+      this.popularCurrenciesRates(this.base);
+    }
+  }
+
+  constructor(private api: ApiService) {}
 
   ngOnInit() {}
+
+  popularCurrenciesRates(baseSymbol: string) {
+    this.api
+      .getPopularCurrenciesRates$(this.api.dateFormat(new Date()), baseSymbol)
+      .subscribe((response) => {
+        this.resultRates = response.rates;
+      });
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ConvertQuery } from '@app/@shared/interfaces/fixer.type';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent{
+export class HomeComponent implements OnInit {
+  quote: string | undefined;
+  isLoading = false;
+  convertQuery!: ConvertQuery;
+
   constructor() {}
+
+  updatePanel(data: ConvertQuery) {
+    this.convertQuery = data;
+  }
+  ngOnInit() {
+    this.isLoading = true;
+  }
 }
